3356e2b13f6598d0089fb2f2065b749a3e66c65e
hobbymarks <ihobbymarks@gmail.com>
2023-10-26 22:52:04 +0800

    add log info

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use dotenv;
+use log::info;
 use reqwest;
 use serde_json::Value;
 
@@ -55,9 +56,57 @@ impl Config {
     }
 }
 
+fn construct_output(config: Config, json: Value) -> Result<String> {
+    let period: &str = match config.period.as_str() {
+        "overall" => "",
+        "7day" => " week",
+        "1month" => " month",
+        "3month" => " 3 months",
+        "6month" => " 6 months",
+        "12month" => " year",
+        _ => return Err(anyhow!("Period {} not allowed. Only allow \"overall\", \"7day\", \"1month\", \"3month\", \"6month\", or \"12month\".", config.period))
+    };
+    info!("period={}", period);
+
+    let mut output: String = format!(
+        "♫ My Top {} played artists in the past{} via #LastFM ♫:\n",
+        config.limit.to_string(),
+        period
+    );
+    info!("output={}", output);
+
+    let artists = json["topartists"]["artist"]
+        .as_array()
+        .ok_or(anyhow!("Error parsing JSON."))?;
+
+    for (i, artist) in artists.iter().enumerate() {
+        info!("i={},artist={}", i, artist);
+        let ending = match i {
+            x if x <= (config.limit as usize - 3) => ",",
+            x if x == (config.limit as usize - 2) => ", &",
+            _ => "",
+        };
+
+        let name = artist["name"]
+            .as_str()
+            .ok_or(anyhow!("Artist not found."))?;
+        let playcount = artist["playcount"]
+            .as_str()
+            .ok_or(anyhow!("Playcount not found."))?;
+
+        output = format!(" {} {} ({}){}", output, name, playcount, ending);
+        info!("output={}", output);
+    }
+
+    info!("output={}", output);
+    Ok(format!("{}.", output))
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::Config;
+    use serde_json::Value;
+
+    use crate::{construct_output, Config};
 
     #[test]
     fn test_config() {
@@ -84,66 +133,65 @@ mod tests {
             assert!(uri.find(pat).is_some());
         }
     }
-}
 
-fn construct_output(config: Config, json: Value) -> Result<String> {
-    let period: &str = match config.period.as_str() {
-        "overall" => "",
-        "7day" => " week",
-        "1month" => " month",
-        "3month" => " 3 months",
-        "6month" => " 6 months",
-        "12month" => " year",
-        _ => return Err(anyhow!("Period {} not allowed. Only allow \"overall\", \"7day\", \"1month\", \"3month\", \"6month\", or \"12month\".", config.period))
-    };
+    // #[test]
+    // fn test_construct_output() {
+    //     let api_key = "api_key";
+    //     let username = "username";
+    //     let limit = 5;
+    //     let period = "7day";
 
-    let mut output: String = format!(
-        "♫ My Top {} played artists in the past{} via #LastFM ♫:\n",
-        config.limit.to_string(),
-        period
-    );
+    //     let config = Config::new(
+    //         String::from(api_key),
+    //         String::from(username),
+    //         limit,
+    //         String::from(period),
+    //     );
 
-    let artists = json["topartists"]["artist"]
-        .as_array()
-        .ok_or(anyhow!("Error parsing JSON."))?;
+    //     let artist = r#"
+    //     {
+    //         "topartists":{
+    //             "artist":["Fia","Sea","Tha","Foa","Fia"]}
+    //     }
+    //     "#;
 
-    for (i, artist) in artists.iter().enumerate() {
-        let ending = match i {
-            x if x <= (config.limit as usize - 3) => ",",
-            x if x == (config.limit as usize - 2) => ", &",
-            _ => "",
-        };
+    //     let parsed_json: Result<Value, serde_json::Error> = serde_json::from_str(artist);
 
-        let name = artist["name"]
-            .as_str()
-            .ok_or(anyhow!("Artist not found."))?;
-        let playcount = artist["playcount"]
-            .as_str()
-            .ok_or(anyhow!("Playcount not found."))?;
-
-        output = format!(" {} {} ({}){}", output, name, playcount, ending);
-    }
-
-    Ok(format!("{}.", output))
+    //     if let Ok(json) = parsed_json {
+    //         let output: Result<String, anyhow::Error> = construct_output(config, json);
+    //         if let Ok(output_string) = output {
+    //             let key = "Fia";
+    //             assert!(output_string.find(key).is_some());
+    //         }
+    //     }
+    // }
 }
 
 fn main() -> Result<()> {
+    env_logger::init();
+    info!("main running ...");
+
     if let Some(home_dir) = dirs::home_dir() {
+        info!("Loading env ...");
         dotenv::from_filename(format!("{}/.config/lfmc/.env", home_dir.to_string_lossy())).ok();
     }
 
+    info!("Parsing args ...");
     let args = Args::parse();
 
+    info!("Creating config ...");
     let config = Config::new(args.api_key, args.username, args.limit, args.period);
 
     let resp: Result<_, reqwest::Error> = reqwest::blocking::get(config.get_uri())?.json::<Value>();
 
     if let Ok(json) = resp {
+        info!("Constructing output ...");
         let output = construct_output(config, json)?;
         println!("\n{}\n", output);
     } else {
         return Err(anyhow!("Could not convert response to JSON."));
     }
 
+    info!("main finished.");
     Ok(())
 }
